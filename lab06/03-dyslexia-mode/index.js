1b08a54c7dc9b374bcbaf177b8de91d15a457fa4
siwoopark10 <57846202+siwoopark10@users.noreply.github.com>
2022-05-06 17:37:32 -0500

    completed lab6 :)

--- lab06/03-dyslexia-mode/index.js
+++ lab06/03-dyslexia-mode/index.js
@@ -3,12 +3,23 @@
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
 */
 
+const initPage = () => {
+  const isPressed = window.localStorage.getItem("dyslexia") === "true";
+  if (isPressed) {
+    document.querySelector('body').className = "dyslexia-mode";
+  }
+}
+
 const changeToDyslexia = () => {
   if (document.querySelector('body').className == 'dyslexia-mode') {
     document.querySelector('body').className = '';
+    window.localStorage.setItem('dyslexia', "false");
   } else {
     document.querySelector('body').className = 'dyslexia-mode';
+    window.localStorage.setItem('dyslexia', "true");
   }
 }
 
 document.querySelector('#dyslexia-toggle').addEventListener('click', changeToDyslexia);
+
+initPage()
